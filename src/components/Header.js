f1b8de2db9ee7ab1d5167cf600e17a7973395aa9
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-29 14:16:40 -0700

    installed and implemented routing and dynamic routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,8 @@
 // CINDY
+import React from 'react';
+
+function Header() {
+  return <div>Header</div>;
+}
+
+export default Header;
---
+++ src/pages/SalesRepresentatives.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SalesRepresentatives() {
+  return <div>SalesRepresentatives</div>;
+}
+
+export default SalesRepresentatives;
