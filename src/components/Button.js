abbcfbe5053b7724018c7d3cbb1ef2fa757c2e45
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-29 12:44:32 -0700

    added button component and cleaned up application

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button({ text, className, id }) {
+  return (
+    <button className={`${className && className} btn`} id={id && id}>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
