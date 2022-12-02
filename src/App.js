1c66742c5fecf413136de0a6f2ffab766dca74ad
cindycas <cha19032@byui.edu>
2022-12-02 13:34:33 -0700

    what I have so far, variables folder created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import Button from './components/Button';
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className='app'>
-      <Button text='Text Inventory' className='btn-margin-4' id='btn1' />
+    <div className="app">
+      <Button text="Text Inventory" className="btn-margin-4" id="btn1" />
+      <Header
+        logo="CAR | "
+        link1="first link, "
+        link2="second link, "
+        link3="third link, "
+        link4="fourth link, "
+        className="sHeader "
+        id="sHeader"
+      />
+      <Header
+        logo="CARSTRO | "
+        link1="first link, "
+        link2="second link, "
+        link3="third link, "
+        link4="fourth link, "
+        className="mlHeader"
+        id="mlHeader"
+      />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,18 @@
 // CINDY
+
+import React from "react";
+
+function Header({ logo, link1, link2, link3, link4, className, id }) {
+  return (
+    <header className={`${className && className} btn`} id={id && id}>
+      {logo}
+      {link1}
+      {link2}
+      {link3}
+      {link4}
+    </header>
+    // <div>Header</div>
+  );
+}
+
+export default Header;
