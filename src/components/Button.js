443e029960c9f33abe4a092324db4c2c0b73e5b7
Kevin <kevin.castro.chavez0@gmail.com>
2022-12-17 01:28:05 -0700

    started to work on the filters component, along with its styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,12 @@
 
 import React from 'react';
 
-function Button({ text, bgColor, className }) {
-  return <button className={`btn btn__${bgColor} ${className}`}>{text}</button>;
+function Button({ text, bgColor, className, onClick }) {
+  return (
+    <button onClick={onClick} className={`btn btn__${bgColor} ${className}`}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
