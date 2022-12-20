89c4978bf0333358258314d9537fae384b385969
Kevin <kevin.castro.chavez0@gmail.com>
2022-12-19 21:38:16 -0700

    set form, inputs, labels and styles for the filter fields

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 
 import Button from '../components/Button';
@@ -23,7 +23,45 @@ function Filters() {
 
       <Button className='filters_button' text={'Clear All'} bgColor='green' />
 
-      <div className='filters_container'>Container</div>
+      <form className='filters_form'>
+        <section>
+          <h5>Brand</h5>
+
+          <div className='filters_checkboxes'>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='kia' name='brands' value='kia' />
+              <label for='kia'>Kia</label>
+            </div>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='bmw' name='brands' value='bmw' />
+              <label for='bmw'>BMW</label>
+            </div>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='audi' name='brands' value='audi' />
+              <label for='audi'>audi</label>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <h5>Model Year</h5>
+
+          <div className='filters_checkboxes'>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='2010' name='brands' value='2010' />
+              <label for='2010'>2010</label>
+            </div>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='2011' name='brands' value='2011' />
+              <label for='2011'>2011</label>
+            </div>
+            <div className='filters_checkbox'>
+              <input type='checkbox' id='2012' name='brands' value='2012' />
+              <label for='2012'>2012</label>
+            </div>
+          </div>
+        </section>
+      </form>
     </section>
   );
 }
