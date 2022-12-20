5f2a5f4a3224d56738d8254d74bb2bc64be4163f
Kevin <kevin.castro.chavez0@gmail.com>
2022-12-19 22:25:02 -0700

    modified mediaqueries, included missing scss files and added some filter logic

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,12 +6,35 @@ import { useStateValue } from '../StateProvider';
 
 function Filters() {
   const [{ showFilters }, dispatch] = useStateValue();
+  const [filtersInfo, setFiltersInfo] = useState({
+    brands: [],
+    years: [],
+  });
+  console.log(filtersInfo);
 
   const hideFilters = () => {
     dispatch({
       type: 'TOGGLE_FILTERS',
       showFilters: false,
     });
+  };
+
+  const handleChange = (e, fieldArray) => {
+    const { value, checked } = e.target;
+    const field = filtersInfo[fieldArray];
+
+    console.log(field);
+    console.log(fieldArray);
+
+    // if (checked) {
+    //   setFiltersInfo({
+    //     fieldArray: [...field, value],
+    //   });
+    // } else {
+    //   setFiltersInfo({
+    //     fieldArray: field.filter((e) => e !== value),
+    //   });
+    // }
   };
 
   return (
@@ -29,15 +52,33 @@ function Filters() {
 
           <div className='filters_checkboxes'>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='kia' name='brands' value='kia' />
+              <input
+                type='checkbox'
+                id='kia'
+                name='brands'
+                value='kia'
+                onChange={(e) => handleChange(e, 'brands')}
+              />
               <label for='kia'>Kia</label>
             </div>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='bmw' name='brands' value='bmw' />
+              <input
+                type='checkbox'
+                id='bmw'
+                name='brands'
+                value='bmw'
+                onChange={(e) => handleChange(e, 'brands')}
+              />
               <label for='bmw'>BMW</label>
             </div>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='audi' name='brands' value='audi' />
+              <input
+                type='checkbox'
+                id='audi'
+                name='brands'
+                value='audi'
+                onChange={(e) => handleChange(e, 'brands')}
+              />
               <label for='audi'>audi</label>
             </div>
           </div>
@@ -48,15 +89,33 @@ function Filters() {
 
           <div className='filters_checkboxes'>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='2010' name='brands' value='2010' />
+              <input
+                type='checkbox'
+                id='2010'
+                name='brands'
+                value='2010'
+                onChange={(e) => handleChange(e, 'years')}
+              />
               <label for='2010'>2010</label>
             </div>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='2011' name='brands' value='2011' />
+              <input
+                type='checkbox'
+                id='2011'
+                name='brands'
+                value='2011'
+                onChange={(e) => handleChange(e, 'years')}
+              />
               <label for='2011'>2011</label>
             </div>
             <div className='filters_checkbox'>
-              <input type='checkbox' id='2012' name='brands' value='2012' />
+              <input
+                type='checkbox'
+                id='2012'
+                name='brands'
+                value='2012'
+                onChange={(e) => handleChange(e, 'years')}
+              />
               <label for='2012'>2012</label>
             </div>
           </div>
