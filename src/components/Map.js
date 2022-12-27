78cf2ed3be146ca451c6cb9f770c4f0810481055
Andres Castro <49927727+andrucastro@users.noreply.github.com>
2022-12-26 21:15:49 -0700

    Andres Sales representatives

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import React from "react";
+import GoogleMapReact from "google-map-react";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function Map() {
   const defaultProps = {
     center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
+      lat: 40.7596198,
+      lng: -111.8867975,
     },
-    zoom: 11,
+    zoom: 5,
   };
 
   return (
     // Important! Always set the container height explicitly
-    <div className='map'>
+    <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyDYhXbjGdmR6Lgd-9FHo4t163UCS4KxdtE' }}
+        bootstrapURLKeys={{ key: "AIzaSyDYhXbjGdmR6Lgd-9FHo4t163UCS4KxdtE" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text='My Marker' />
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
       </GoogleMapReact>
     </div>
   );
