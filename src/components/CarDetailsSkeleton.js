cd24bfe04fd13b3da43529782e6d62116db9b437
Kevin <kevin.castro.chavez0@gmail.com>
2022-12-31 03:36:22 -0700

    finished styles for the carDetails skeleton for tablet and desktop view

--- src/components/CarDetailsSkeleton.js
+++ src/components/CarDetailsSkeleton.js
@@ -7,56 +7,60 @@ function CarDetailsSkeleton() {
     <div className='carDetailsSkeleton'>
       <Stack spacing={1}>
         <Skeleton variant='rounded' width='100%' height={120} />
-        <Skeleton variant='text' sx={{ fontSize: '18px' }} width='40%' />
+        <Skeleton variant='text' sx={{ fontSize: '18px' }} width='100px' />
 
-        <Skeleton
-          className='carDetailsSkeleton_title'
-          variant='rounded'
-          width='50%'
-          height={24}
-        />
-        <Skeleton variant='rounded' width='60%' height={16} />
-
-        <div className='carDetailsSkeleton_price'>
-          <Skeleton variant='rounded' width='40%' height={48} />
-
-          <div className='carDetailsSkeleton_price-offer'>
-            <Skeleton variant='rounded' width='100%' height={20} />
-            <Skeleton variant='rounded' width='100%' height={20} />
+        <div className='carDetailsSkeleton_info'>
+          <div className='carDetailsSkeleton_info-top'>
+            <Skeleton
+              className='carDetailsSkeleton_title'
+              variant='rounded'
+              width='50%'
+              height={24}
+            />
+            <Skeleton variant='rounded' width='60%' height={16} />
           </div>
-        </div>
 
-        <Skeleton variant='rounded' width='100%' height={52} />
+          <div className='carDetailsSkeleton_info-price'>
+            <Skeleton variant='rounded' width='40%' height={48} />
+
+            <div className='carDetailsSkeleton_info-price-offer'>
+              <Skeleton variant='rounded' width='100%' height={20} />
+              <Skeleton variant='rounded' width='100%' height={20} />
+            </div>
+          </div>
+
+          <Skeleton variant='rounded' width='100%' height={52} />
+        </div>
 
         <div className='carDetailsSkeleton_iconsContainer'>
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
 
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
 
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
 
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
 
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
 
           <div className='carDetailsSkeleton_icon'>
-            <Skeleton variant='rounded' width={60} height={60} />
-            <Skeleton variant='rounded' width={60} height={18} />
+            <Skeleton variant='rounded' width={80} height={80} />
+            <Skeleton variant='rounded' width={80} height={18} />
           </div>
         </div>
       </Stack>
