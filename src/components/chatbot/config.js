a4b7ec5f4d4e9042a20b173e5cd411589dd5b60c
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-04 00:37:45 -0700

    applied some styles to make it look better

--- src/components/chatbot/config.js
+++ src/components/chatbot/config.js
@@ -3,7 +3,9 @@ import { createChatBotMessage } from 'react-chatbot-kit';
 const config = {
   botName: 'Digital Assistant',
   initialMessages: [
-    createChatBotMessage('Welcome to Carstro chat! How can we help you'),
+    createChatBotMessage(
+      'Welcome to Carstro chat! How can we help you. Welcome to Carstro chat! How can we help you'
+    ),
   ],
 };
 
