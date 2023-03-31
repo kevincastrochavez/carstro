3935c1a964db23533b064c14439ad5dcebc87223
Kevin Castro <kevincastro@C02FD2RV-0db637.local>
2023-03-31 17:15:26 -0600

    added more optimized images and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Fab } from '@mui/material';
 import TextsmsIcon from '@mui/icons-material/Textsms';
 
 import Header from './components/Header';
-import Homepage from './pages/Homepage';
+// import Homepage from './pages/Homepage';
 import Inventory from './pages/Inventory';
 import Marketing from './pages/Marketing';
 import SalesRepresentatives from './pages/SalesRepresentatives';
@@ -13,10 +11,13 @@ import CarDetails from './components/CarDetails';
 import Footer from './components/Footer';
 import Chatbot from 'react-chatbot-kit';
 
+import { lazy, Suspense } from 'react';
 import config from './components/chatbot/config';
 import ActionProvider from './components/chatbot/ActionProvider';
 import MessageParser from './components/chatbot/MessageParser';
 import { useStateValue } from './StateProvider';
+
+const Homepage = lazy(() => import('./pages/Homepage'))
 
 function App() {
   const [{ showChat }, dispatch] = useStateValue();
@@ -46,7 +47,11 @@ function App() {
         )}
 
         <Routes>
-          <Route exact path='/' element={<Homepage />} />
+          <Route exact path='/' element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Homepage />
+            </Suspense>
+          } />
           <Route path='/inventory' element={<Inventory />} />
           <Route path='/inventory/:id' element={<CarDetails />} />
           <Route path='/marketing' element={<Marketing />} />
